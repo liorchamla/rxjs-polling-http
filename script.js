c89dcf64627af18998f4932d03a10584c95fe4fa
Lior Chamla <lchamla@gmail.com>
2022-03-03 14:34:33 +0000

    Added API URL

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const startButton = document.querySelector("#start");
 const stopButton = document.querySelector("#stop");
 const status = document.querySelector("#status");
 const img = document.querySelector("img");
+
+// API :
+const API_URL = "https://placekitten.com/600/400";
