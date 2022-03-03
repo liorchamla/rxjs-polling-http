c838dfc3925a17c437c7eb41444812a5aece33d5
Lior Chamla <lchamla@gmail.com>
2022-03-03 14:33:28 +0000

    Mise en place

---
+++ script.js
@@ -0,0 +1,5 @@
+// elements html
+const startButton = document.querySelector("#start");
+const stopButton = document.querySelector("#stop");
+const status = document.querySelector("#status");
+const img = document.querySelector("img");
